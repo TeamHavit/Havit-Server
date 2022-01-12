ed80e83005e4f9b28ac2de08a05ab6cb4f9b8579
Hyosik Philip Joo <lux1937@gmail.com>
2022-01-13 03:53:24 +0900

    [FEAT] 콘텐츠 스크랩 api 로직 구현

---
+++ functions/api/routes/content/scrap.js
@@ -0,0 +1,39 @@
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const ogs = require('open-graph-scraper');
+
+module.exports = async(req, res) => {
+    const {link} = req.body; // 클라이언트에게 받은 url 링크
+    
+    if(!link) { // url 링크가 존재하지 않을 때
+      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    }
+
+    try {
+        const data = await ogs({ url : link });
+        let response;
+        if (!data.result.ogImage) { // 이미지가 존재하지 않는 경우
+          response = {
+            ogTitle: data.result.ogTitle,
+            ogDescription: data.result.ogDescription,
+            ogImage: '',
+            ogUrl: data.result.ogUrl,
+            }
+        }
+        else { // 이미지가 존재하는 경우
+          response = {
+            ogTitle: data.result.ogTitle,
+            ogDescription: data.result.ogDescription,
+            ogImage: data.result.ogImage,
+            ogUrl: data.result.ogUrl,
+            }
+        }
+        console.log(response);
+        res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SCRAP_CONTENT_SUCCESS, response));
+    } catch(error) {
+        console.log(error);
+    }
+  
+    
+}
