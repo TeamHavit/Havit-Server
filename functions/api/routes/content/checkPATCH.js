ef5b232b1d1e0049edf8076a0f30d889419bd93f
Hyosik Philip Joo <lux1937@gmail.com>
2022-01-14 02:39:38 +0900

    [CHORE] 코딩 컨벤션 수정, Slack API 에러 체크 로직 추가

--- functions/api/routes/content/checkPATCH.js
+++ functions/api/routes/content/checkPATCH.js
@@ -16,7 +16,9 @@ module.exports = async (req, res) => {
 
   const {contentId} = req.body
   
-  if (!contentId) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  if (!contentId) {
+    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  }
   
   let client;
 
@@ -30,6 +32,8 @@ module.exports = async (req, res) => {
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
+    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.user ? `uid:${req.user.userId}` : 'req.user 없음'} ${JSON.stringify(error)}`;
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
     
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
     
