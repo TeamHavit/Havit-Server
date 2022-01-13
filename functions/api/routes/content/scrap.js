1fde79b2fb1b97b08e3af35ba26f03f8fd625efa
Hyosik Philip Joo <lux1937@gmail.com>
2022-01-13 20:57:58 +0900

    [CHORE] 코딩 컨벤션 수정, API 설명 주석 추가

--- functions/api/routes/content/scrap.js
+++ functions/api/routes/content/scrap.js
@@ -3,17 +3,25 @@ const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const ogs = require('open-graph-scraper');
 
+/**
+ *  @route GET /content/scrap
+ *  @desc 콘텐츠 스크랩
+ *  @access Private
+ */
+
 module.exports = async(req, res) => {
     const {link} = req.body; // 클라이언트에게 받은 url 링크
     
-    if(!link) { // url 링크가 존재하지 않을 때
+    if (!link) { 
+      // url 링크가 존재하지 않을 때
       return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
     }
 
     try {
         const data = await ogs({ url : link });
         let response;
-        if (!data.result.ogImage) { // 이미지가 존재하지 않는 경우
+        if (!data.result.ogImage) { 
+          // 이미지가 존재하지 않는 경우
           response = {
             ogTitle: data.result.ogTitle,
             ogDescription: data.result.ogDescription,
@@ -21,7 +29,8 @@ module.exports = async(req, res) => {
             ogUrl: data.result.ogUrl,
             }
         }
-        else { // 이미지가 존재하는 경우
+        else { 
+          // 이미지가 존재하는 경우
           response = {
             ogTitle: data.result.ogTitle,
             ogDescription: data.result.ogDescription,
