5e7347351adce1739699d45567af5d0bbdd93a07
Hyosik Philip Joo <lux1937@gmail.com>
2022-01-15 17:34:23 +0900

    [FEAT] contentUnseenListGET DB 로직 구현

--- functions/db/content.js
+++ functions/db/content.js
@@ -131,4 +131,22 @@ const getRecentContents = async (client, userId) => {
     return convertSnakeToCamel.keysToCamel(rows);
 };
 
-module.exports = { addContent, toggleContent, getAllContents, searchContent, updateContentIsDeleted, getRecentContents };
+const getUnseenContents = async (client, userId) => {
+    const { rows } = await client.query(
+        `
+        SELECT c.id, c.title, c.description, c.image, c.url, c.is_seen, c.is_notified, c.created_at, n.notification_time
+        FROM content c
+        JOIN notification n on c.id = n.content_id
+        WHERE c.user_id = $1 AND c.is_deleted = FALSE AND c.is_notified = true AND c.is_seen = FALSE
+        UNION ALL
+        SELECT c.id, c.title, c.description, c.image, c.url, c.is_seen, c.is_notified, c.created_at, null as notification_time
+        FROM content c
+        WHERE c.user_id = $1 AND c.is_deleted = FALSE AND c.is_notified = false AND c.is_seen = FALSE
+        ORDER BY created_at
+        `,
+        [userId]
+    );
+    return convertSnakeToCamel.keysToCamel(rows);
+};
+
+module.exports = { addContent, toggleContent, getAllContents, searchContent, updateContentIsDeleted, getRecentContents, getUnseenContents };
