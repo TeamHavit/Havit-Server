3f4fff7137a582cb690d23076691d2a87de3a3d9
jokj624 <jokj624@gmail.com>
2022-01-18 21:45:43 +0900

    [FIX] order_index 수정

--- functions/api/routes/category/categoryPOST.js
+++ functions/api/routes/category/categoryPOST.js
@@ -13,6 +13,8 @@ const { categoryDB } = require('../../../db');
  */
 module.exports = async (req, res) => {
     const { title, imageId } = req.body;
+    let newIndex = 0;
+
     if (!title || !imageId) {
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
     }
@@ -24,8 +26,12 @@ module.exports = async (req, res) => {
         client = await db.connect(req);
 
         const oldCategory = await categoryDB.getAllCategories(client, userId);
-        const newIndex = oldCategory[oldCategory.length-1].orderIndex + 1; // 새 카테고리 인덱스는 마지막 카테고리 인덱스 + 1
-        const category = await categoryDB.addCategory(client, userId, title, imageId, 0, newIndex);
+
+        if(oldCategory.length) {
+            newIndex = oldCategory[oldCategory.length - 1].orderIndex + 1; // 새 카테고리 인덱스는 마지막 카테고리 인덱스 + 1
+        }
+
+        const category = await categoryDB.addCategory(client, userId, title, imageId, newIndex);
         
         res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, responseMessage.ADD_ONE_CATEGORY_SUCCESS));
     } catch (error) {
