74261cdfe2f6691da31033d6c318bef281b35e18
Hyosik Philip Joo <lux1937@gmail.com>
2022-01-20 19:08:25 +0900

    [FEAT] user 라우트 분기

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use('/content', require('./content'));
 router.use('/category', require('./category'));
 router.use('/recommendation', require('./recommendation'));
+router.use('/user', require('./user'));
 
 module.exports = router;
