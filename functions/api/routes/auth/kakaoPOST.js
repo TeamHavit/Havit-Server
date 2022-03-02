e90a6e50ba11880af004b068427a445218f5dbe2
Hyosik Philip Joo <lux1937@gmail.com>
2022-03-02 23:29:54 +0900

    [CHORE] 카카오 소셜 로그인 API 출력문 삭제

--- functions/api/routes/auth/kakaoPOST.js
+++ functions/api/routes/auth/kakaoPOST.js
@@ -11,18 +11,13 @@ const { response } = require('express');
  */
 
 module.exports = async (req, res) => {
-    const kakaoToken = req.body.token; // 클라이언트에게 전달받은 Kakao Access Token
-    //console.log(kakaoToken);
+    const kakaoAccessToken = req.body.token; // 클라이언트에게 전달받은 Kakao Access Token
 
-    if (!kakaoToken) {
+    if (!kakaoAccessToken) {
       // Kakao Access Token이 없는 경우
       return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.TOKEN_EMPTY));
     }
-  
-    //console.log(`Verifying Kakao token: ${kakaoToken}`);
 
-    const firebaseToken = await kakao.createFirebaseToken(kakaoToken);
-    console.log(firebaseToken); 
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.KAKAO_LOGIN_SUCCESS, firebaseToken));
-
-}
+    const firebaseUserData = await kakao.createFirebaseToken(kakaoAccessToken);
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.KAKAO_LOGIN_SUCCESS));
+};
