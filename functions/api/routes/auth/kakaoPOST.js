5d47b95feb0278683823fa1d217f746feaab9c54
Hyosik Philip Joo <lux1937@gmail.com>
2022-03-03 23:43:59 +0900

    [FEAT] 카카오 소셜 로그인 API DB 유저 추가 로직 추가

--- functions/api/routes/auth/kakaoPOST.js
+++ functions/api/routes/auth/kakaoPOST.js
@@ -1,8 +1,11 @@
 const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
-const kakao = require('../../../middlewares/kakaoAuth');
-const { response } = require('express');
+const kakao = require('../../../lib/kakaoAuth');
+const db = require('../../../db/db');
+const { userDB } = require('../../../db');
+const functions = require('firebase-functions');
+const slackAPI = require('../../../middlewares/slackAPI');
 
 /**
  *  @route POST /auth/kakao
@@ -19,5 +22,26 @@ module.exports = async (req, res) => {
     }
 
     const firebaseUserData = await kakao.createFirebaseToken(kakaoAccessToken);
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.KAKAO_LOGIN_SUCCESS));
+    const { firebaseToken, firebaseUserId, nickname, email } = firebaseUserData;
+
+    let client;
+  
+    try {
+      client = await db.connect(req);
+  
+      const user = await userDB.addUser(client, firebaseUserId, nickname, email);
+      
+      res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.KAKAO_LOGIN_SUCCESS, { user, firebaseToken }));
+      
+    } catch (error) {
+      functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+      console.log(error);
+      const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.user ? `uid:${req.user.userId}` : 'req.user 없음'} ${JSON.stringify(error)}`;
+      slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
+      
+      res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+      
+    } finally {
+      client.release();
+    }
 };
