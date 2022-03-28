96efdeae97d0b99e01d0ca91f70fb30f21c0a64c
Hyosik Philip Joo <lux1937@gmail.com>
2022-03-28 19:08:50 +0900

    [DEL] 콘텐츠 스크랩 API 분기 삭제

--- functions/api/routes/content/index.js
+++ functions/api/routes/content/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { checkUser } = require('../../../middlewares/auth');
 
-router.get('/scrap', checkUser, require('./contentScrapGET'));
 router.post('/', checkUser, require('./contentPOST'));
 router.get('/', checkUser, require('./contentListGET'));
 router.get('/search', checkUser, require('./contentSearchGET'));
