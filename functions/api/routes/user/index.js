e1aea9b07e4f9adfc90d0a6a5fd3708dd4876996
jokj624 <jokj624@gmail.com>
2022-04-12 23:13:54 +0900

    [FEAT] 유저 fcm token 리프레시 API 라우터 연결

--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { checkUser } = require('../../../middlewares/auth');
 
 router.get('/', checkUser, require('./userGET'));
+router.put('/fcm-token', checkUser, require('./userUpdateFcmTokenPUT'));
 
 module.exports = router;
