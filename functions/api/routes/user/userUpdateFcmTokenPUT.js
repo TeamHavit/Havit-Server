00f1e431517249fcbcae5e399790a0561395bfd2
jokj624 <jokj624@gmail.com>
2022-04-12 23:13:43 +0900

    [FEAT] 유저 fcm token 리프레시 API

---
+++ functions/api/routes/user/userUpdateFcmTokenPUT.js
@@ -0,0 +1,50 @@
+const functions = require('firebase-functions');
+const slackAPI = require('../../../middlewares/slackAPI');
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const db = require('../../../db/db');
+const { userDB, contentDB, categoryDB } = require('../../../db');
+const { default: axios } = require('axios');
+
+module.exports = async (req, res) => {
+    const { fcmToken } = req.body;
+    const { userId } = req.user;
+
+    if (!fcmToken) {
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    }
+    console.log(userId)
+    let client;
+
+    try {
+        client = await db.connect(req);
+
+        const user = await userDB.getUser(client, userId);
+
+        if (!user.mongoUserId) {
+            return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, responseMessage.NOT_FOUND));
+        }
+
+        const updatedToken = await axios.put(process.env.PUSH_SERVER_URL+`user/${user.mongoUserId}/refresh-token`, {
+            fcmToken
+        });
+
+        if (updatedToken.status != 204) {
+            return res.status(updatedToken.statusCode).send(util.fail(updatedToken.statusCode, updatedToken.statusText));
+        }
+
+        res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ONE_USER_SUCCESS));
+
+    } catch (error) {
+        functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+        console.log(error);
+        const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.user ? `uid:${req.user.userId}` : 'req.user 없음'} ${JSON.stringify(error)}`;
+        slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
+
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+
+    } finally {
+        client.release();
+    }
+};
