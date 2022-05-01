ce9cfb7ab97678d171fa8c21453156610ee979d4
Hyosik Philip Joo <lux1937@gmail.com>
2022-05-02 02:58:07 +0900

    [FEAT] 애플 소셜 로그인 라우트 분기

--- functions/api/routes/auth/index.js
+++ functions/api/routes/auth/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.post('/kakao', require('./kakaoPOST'));
 router.post('/reissue', require('./reissueTokenPOST'));
+router.post('/apple', require('./applePOST'));
 
 module.exports = router;
