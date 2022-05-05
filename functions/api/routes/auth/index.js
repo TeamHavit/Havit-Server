49da93d1388de66a5dc31b078cec463bc1cf8566
Hyosik Philip Joo <lux1937@gmail.com>
2022-05-05 16:30:57 +0900

    [FIX] 토큰 재발급 API 엔드포인트 Restful하게 수정

--- functions/api/routes/auth/index.js
+++ functions/api/routes/auth/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/kakao', require('./kakaoPOST'));
-router.post('/reissue', require('./reissueTokenPOST'));
+router.post('/token', require('./reissueTokenPOST'));
 router.post('/apple', require('./applePOST'));
 
 module.exports = router;
