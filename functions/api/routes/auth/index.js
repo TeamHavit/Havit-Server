88bcff191139e18679d0549224f79ac0dee45a04
Hyosik Philip Joo <lux1937@gmail.com>
2022-05-08 17:49:39 +0900

    [FEAT] auth 라우터 signup 추가

--- functions/api/routes/auth/index.js
+++ functions/api/routes/auth/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/signin', require('./signinPOST'));
+router.post('/signup', require('./signupPOST'));
 router.post('/token', require('./reissueTokenPOST'));
 
 module.exports = router;
