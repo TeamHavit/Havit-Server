61eedc5879aba39e7768244e994348a44df53be3
Hyosik Philip Joo <lux1937@gmail.com>
2022-05-16 22:54:08 +0900

    [FEAT] 회원탈퇴 API 라우트 분기

--- functions/api/routes/auth/index.js
+++ functions/api/routes/auth/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.post('/signin', require('./signinPOST'));
 router.post('/signup', require('./signupPOST'));
 router.post('/token', require('./reissueTokenPOST'));
+router.post('/delete', require('./deleteUserPOST'));
 
 module.exports = router;
