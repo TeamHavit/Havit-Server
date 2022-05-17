e42c7ca893dedadc6bac579667c44964a3688ca6
Hyosik Philip Joo <lux1937@gmail.com>
2022-05-17 20:40:57 +0900

    [FEAT] 회원 탈퇴 API 404 에러 추가

--- functions/api/routes/auth/deleteUserPOST.js
+++ functions/api/routes/auth/deleteUserPOST.js
@@ -21,18 +21,27 @@ module.exports = async (req, res) => {
   try {
     client = await db.connect(req);
 
-    const deleteUser = await userDB.getUser(client, userId);
-    await getAuth().deleteUser(deleteUser.idFirebase);
-    await userDB.deleteUser(client, userId);
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.DELETE_USER));
+    const deleteUser = await userDB.getUser(client, userId); // DB에서 해당 유저 정보 받아 옴
 
+    try {
+      await getAuth().getUser(deleteUser.idFirebase); // Firebase Auth에서 해당 유저 존재 유무 확인
+    } catch (error) {
+      functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+      console.log(error);
+      const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.user ? `uid:${req.user.userId}` : 'req.user 없음'} ${JSON.stringify(error)}`;
+      slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
+      return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, responseMessage.NO_USER)); // Firebase Auth에 해당 유저 존재하지 않을 경우 : 404 NOT FOUND
+    }
+
+    await getAuth().deleteUser(deleteUser.idFirebase); // Firebase Auth에서 해당 유저 삭제
+    await userDB.deleteUser(client, userId); // DB에서 해당 유저 삭제
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.DELETE_USER));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
     const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.user ? `uid:${req.user.userId}` : 'req.user 없음'} ${JSON.stringify(error)}`;
     slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
-    
-    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR)); // DB 관련 에러 : 500 INTERNAL SERVER ERROR
     
   } finally {
     client.release();
