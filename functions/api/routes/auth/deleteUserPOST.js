b1f2310b0666ac52195fd5418b327f91dce6eddc
Hyosik Philip Joo <lux1937@gmail.com>
2022-05-18 16:36:26 +0900

    [FEAT] Firebase Auth 에러 404와 500로 분기

--- functions/api/routes/auth/deleteUserPOST.js
+++ functions/api/routes/auth/deleteUserPOST.js
@@ -20,7 +20,6 @@ module.exports = async (req, res) => {
   let deleteUser;
   
   try {
-    // DB 통신
     client = await db.connect(req);
     deleteUser = await userDB.getUser(client, userId); // DB에서 해당 유저 정보 받아 옴    
   } catch (error) {
@@ -28,19 +27,23 @@ module.exports = async (req, res) => {
     console.log(error);
     const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.user ? `uid:${req.user.userId}` : 'req.user 없음'} ${JSON.stringify(error)}`;
     slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
-    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR)); // DB 관련 에러 : 500 INTERNAL SERVER ERROR
+    client.release();
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR)); // DB 관련 에러 : 500 INTERNAL SERVER ERROR
   } 
-  
+
   try {
-    // Firebase Auth와 통신
-    // await getAuth().getUser(deleteUser.idFirebase); // delete로도 404 잡을 수 있어서 getUser 굳이 한번 더 할 필요가 없을 것 같은뎅
     await getAuth().deleteUser(deleteUser.idFirebase); // Firebase Auth에서 해당 유저 삭제
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
     const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.user ? `uid:${req.user.userId}` : 'req.user 없음'} ${JSON.stringify(error)}`;
     slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
-    return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, responseMessage.NO_USER)); // Firebase Auth에 해당 유저 존재하지 않을 경우 : 404 NOT FOUND
+    client.release();
+    if (error.errorInfo.code === 'auth/user-not-found') {
+      return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, responseMessage.NO_USER)); // Firebase Auth에 해당 유저 존재하지 않을 경우 : 404 NOT FOUND
+    } else {
+      return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR)); // 기타 Firebase Auth 에러 : 500 INTERNAL SERVER ERROR
+    }
   }
 
   try {
@@ -53,7 +56,6 @@ module.exports = async (req, res) => {
     slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR)); // DB 관련 에러 : 500 INTERNAL SERVER ERROR 
   }
-
   finally {
     client.release();
   }
