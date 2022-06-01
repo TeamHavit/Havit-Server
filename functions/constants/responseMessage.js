6f43b355ef193e3dc010b75af22539d710e9d736
jokj624 <jokj624@gmail.com>
2022-06-01 21:24:53 +0900

    [CHORE] 콘텐츠 알림 조회 responseMessage 추가

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -33,6 +33,7 @@ module.exports = {
     UPDATE_CONTENT_CATEGORY_SUCCESS: '콘텐츠 카테고리 변경 성공',
     UPDATE_CONTENT_NOTIFICATION_SUCCESS: '콘텐츠 알림 변경 성공',
     DUPLICATED_CONTENT: '중복된 콘텐츠',
+    READ_CONTENT_NOTIFICATION_SUCCESS: '콘텐츠 알림 조회 성공',
 
     // 카테고리
     ADD_ONE_CATEGORY_SUCCESS: '카테고리 생성 완료',
