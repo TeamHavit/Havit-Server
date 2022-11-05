c25bf63521b4d37cf39cb0318791a592ad3a3e38
Hyosik Philip Joo <lux1937@gmail.com>
2022-11-05 13:26:17 +0900

    [FEAT] node_env에 따라 다른 환경변수 사용하도록 설정

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./havit-wesopt29-firebase-adminsdk-mgljp-478046b091');
+const devServiceAccount = require('./havit-wesopt29-firebase-adminsdk-mgljp-478046b091');
+const prodServiceAccount = require('./havit-production-firebase-adminsdk-bypl1-d081cc62e4');
 const dotenv = require('dotenv');
 
-dotenv.config();
+let path;
+let serviceAccount;
+switch (process.env.NODE_ENV) {
+  case "prod":
+    path = `${__dirname}/.env.prod`;
+    serviceAccount = prodServiceAccount;
+    break;
+  case "dev":
+    path = `${__dirname}/.env.dev`;
+    serviceAccount = devServiceAccount;
+    break;
+  default:
+    path = `${__dirname}/.env.dev`;
+}
+dotenv.config({ path: path });
 
 let firebase;
 
@@ -10,8 +25,8 @@ if (admin.apps.length === 0) {
   firebase = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
-} else {
-  firebase = admin.app();
+  } else {
+    firebase = admin.app();
 }
 
 module.exports = {
