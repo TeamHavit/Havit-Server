a91176b6ea65ba6a2a3e8f3881e5881d28ec4a52
Hyosik Philip Joo <lux1937@gmail.com>
2022-11-11 10:53:54 +0900

    [FIX] serviceAccount 파일 형식 명시적으로 지정

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,9 @@
 const admin = require('firebase-admin');
-const devServiceAccount = require('./havit-wesopt29-firebase-adminsdk-mgljp-478046b091');
-const prodServiceAccount = require('./havit-production-firebase-adminsdk-bypl1-d081cc62e4');
+const devServiceAccount = require('./havit-wesopt29-firebase-adminsdk-mgljp-478046b091.json');
+const prodServiceAccount = require('./havit-production-firebase-adminsdk-bypl1-d081cc62e4.json');
 const dotenv = require('dotenv');
 
 let path;
-let serviceAccount;
 switch (process.env.NODE_ENV) {
   case "production":
     path = `${__dirname}/.env.prod`;
