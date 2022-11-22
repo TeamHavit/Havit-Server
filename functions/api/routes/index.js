44b522bb142568cefd45302db710157d4d9d443b
Hyosik Philip Joo <lux1937@gmail.com>
2022-11-23 00:57:01 +0900

    [FEAT] Health 라우터 분기

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -7,5 +7,6 @@ router.use('/recommendation', require('./recommendation'));
 router.use('/user', require('./user'));
 router.use('/auth', require('./auth'));
 router.use('/notice', require('./notice'));
+router.use('/health', require('./health'));
 
 module.exports = router;
