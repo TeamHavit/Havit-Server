0f3178896e77db544e30b214bf969f24826cd01e
jokj624 <jokj624@gmail.com>
2023-01-27 19:16:02 +0900

    Revert "Merge branch 'hotfix/20230127-3'"
    This reverts commit 6da2b323cdda48918a83d1e41c95d88d6a26e172, reversing
    changes made to 14604aed0b3ceba9d49453ff4dcbe6ec664db17a.

--- functions/lib/appleAuth.js
+++ functions/lib/appleAuth.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const jwt = require('jsonwebtoken'); 
 const functions = require("firebase-functions");
 const slackAPI = require("../middlewares/slackAPI");
-const fs = require('fs').promises;
+const fs = require('fs');
 const { resolve } = require('path');
 const qs = require('qs');
 
@@ -11,9 +11,7 @@ dotenv.config();
 
 const appleBaseURL = "https://appleid.apple.com";
 
-const getPrivateKey = async () => {
-    return await fs.readFile(resolve(__dirname, `../${process.env.APPLE_PRIVATE_KEY_FILE}`), 'utf8');
-}
+const privateKey = fs.readFileSync(resolve(__dirname, `../${process.env.APPLE_PRIVATE_KEY_FILE}`), 'utf8');
 
 const header = {
     kid: process.env.APPLE_KEY_ID,
@@ -32,7 +30,7 @@ const payload = {
  *  @param {String} appleCode
  */
 const getAppleRefreshToken = async (appleCode) => {
-    const clientSecret = jwt.sign(payload, getPrivateKey(), {
+    const clientSecret = jwt.sign(payload, privateKey, {
         algorithm: 'ES256',
         header
     });
@@ -61,7 +59,7 @@ const getAppleRefreshToken = async (appleCode) => {
  * @param {String} appleRefreshToken
  */
 const revokeAppleToken = async (appleRefreshToken) => {
-    const clientSecret = jwt.sign(payload, getPrivateKey(), {
+    const clientSecret = jwt.sign(payload, privateKey, {
         algorithm: 'ES256',
         header
     });
