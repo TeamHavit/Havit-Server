6cfe815223aabf81223cac4913aa2a46177580cc
Hyosik <hyosik@chartmetric.com>
2023-05-30 15:59:11 -0500

    [FEAT] 500 에러 의외의 에러도 에러 핸들러에서 처리 가능하도록 수정

--- functions/middlewares/errorHandler.js
+++ functions/middlewares/errorHandler.js
@@ -5,10 +5,12 @@ const statusCode = require('../constants/statusCode');
 const responseMessage = require('../constants/responseMessage');
 
 module.exports = (error, req, res, next) => {
+  const errorStatusCode = error.statusCode ? error.statusCode : statusCode.INTERNAL_SERVER_ERROR;
+  const errorResponseMessage = error.responseMessage ? error.responseMessage : responseMessage.INTERNAL_SERVER_ERROR;
   functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
   console.log(error);
   const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.user ? `uid:${req.user.userId}` : 'req.user 없음'} ${JSON.stringify(error)}`;
   slackAPI.sendMessageToSlack(slackMessage, slackAPI.WEB_HOOK_ERROR_MONITORING);
 
-  res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  res.status(errorStatusCode).send(util.fail(errorStatusCode, errorResponseMessage));
 }
