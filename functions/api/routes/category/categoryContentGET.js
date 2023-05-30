3cdb6e7d994f075bee3f71b39c26bbdc95471aaf
Hyosik <hyosik@chartmetric.com>
2023-05-30 17:05:29 -0500

    [FEAT] 카테고리 별 컨텐츠 조회 API 에러 핸들링 적용

--- functions/api/routes/category/categoryContentGET.js
+++ functions/api/routes/category/categoryContentGET.js
@@ -1,13 +1,12 @@
 const _ = require('lodash');
-const functions = require('firebase-functions');
 const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
-const slackAPI = require('../../../middlewares/slackAPI');
 const db = require('../../../db/db');
 const { categoryContentDB } = require('../../../db');
 const dayjs = require('dayjs');
 const customParseFormat = require('dayjs/plugin/customParseFormat');
+const asyncWrapper = require('../../../lib/asyncWrapper');
 
 /**
  *  @route GET /category/:categoryId
@@ -15,70 +14,57 @@ const customParseFormat = require('dayjs/plugin/customParseFormat');
  *  @access Private
  */
 
-module.exports = async (req, res) => {
+module.exports = asyncWrapper(async (req, res) => {
     const { userId } = req.user;
     const { categoryId } = req.params;
     const { option, filter } = req.query;
     let contents = {};
-    let client;
+    let dbConnection = await db.connect(req);
+    req.dbConnection = dbConnection;
 
     dayjs().format();
     dayjs.extend(customParseFormat);
 
-    try {
-        client = await db.connect(req);
-        
-        if (option == "all") {
-            // 전체 조회
-            contents = await categoryContentDB.getAllCategoryContentByFilter(client, userId, categoryId, filter);
-        } else if (option == "notified") {
-            // 알림 설정된 콘텐츠만 조회
-            contents = await categoryContentDB.getCategoryContentByFilterAndNotified(client, userId, categoryId, true, filter)
+    if (option == "all") {
+        // 전체 조회
+        contents = await categoryContentDB.getAllCategoryContentByFilter(dbConnection, userId, categoryId, filter);
+    } else if (option == "notified") {
+        // 알림 설정된 콘텐츠만 조회
+        contents = await categoryContentDB.getCategoryContentByFilterAndNotified(dbConnection, userId, categoryId, true, filter)
 
-        } else {
-            // is_seen에 따라 조회
-            contents = await categoryContentDB.getCategoryContentByFilterAndSeen(client, userId, categoryId, option, filter);
-        }
-        if (filter == "reverse") {
-            // DESC를 이용했으므로 다시 reverse
-            contents = contents.reverse();
-        }
-        if (filter == "seen_at") {
-            // 최근 조회 순 기준인 경우, 조회하지 않은 콘텐츠 제외
-            const removedElements = _.remove(contents, function(content) {
-                return content.isSeen === false;
-            });
-        }
-
-        const result = await Promise.all(contents.map(content => {
-            /**
-             * 클라이언트가 사용할 createdAt, notificationTime, seenAt 은 day js로 format 수정
-             * 시간이 null이면 빈 문자열로 수정
-             */
-            content.createdAt = dayjs(`${content.createdAt}`).format("YYYY-MM-DD HH:mm");
-            if (content.notificationTime) {
-                content.notificationTime = dayjs(`${content.notificationTime}`).format("YYYY-MM-DD HH:mm");
-            } else {
-                content.notificationTime = "";
-            }
-            if (content.seenAt) {
-                content.seenAt = dayjs(`${content.seenAt}`).format("YYYY-MM-DD HH:mm");
-            } else {
-                content.seenAt = "";
-            }
-            return content;
-        }));
-
-        res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_CATEGORY_CONTENT_SUCCESS, result));
-
-    } catch (error) {
-        console.log(error);
-        functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
-        const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.user ? `uid:${req.user.userId}` : 'req.user 없음'} ${JSON.stringify(error)}`;
-        slackAPI.sendMessageToSlack(slackMessage, slackAPI.WEB_HOOK_ERROR_MONITORING);
-
-        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-    } finally {
-        client.release();
+    } else {
+        // is_seen에 따라 조회
+        contents = await categoryContentDB.getCategoryContentByFilterAndSeen(dbConnection, userId, categoryId, option, filter);
     }
-};
+    if (filter == "reverse") {
+        // DESC를 이용했으므로 다시 reverse
+        contents = contents.reverse();
+    }
+    if (filter == "seen_at") {
+        // 최근 조회 순 기준인 경우, 조회하지 않은 콘텐츠 제외
+        _.remove(contents, function(content) {
+            return content.isSeen === false;
+        });
+    }
+
+    const result = await Promise.all(contents.map(content => {
+        /**
+         * 클라이언트가 사용할 createdAt, notificationTime, seenAt 은 day js로 format 수정
+         * 시간이 null이면 빈 문자열로 수정
+         */
+        content.createdAt = dayjs(`${content.createdAt}`).format("YYYY-MM-DD HH:mm");
+        if (content.notificationTime) {
+            content.notificationTime = dayjs(`${content.notificationTime}`).format("YYYY-MM-DD HH:mm");
+        } else {
+            content.notificationTime = "";
+        }
+        if (content.seenAt) {
+            content.seenAt = dayjs(`${content.seenAt}`).format("YYYY-MM-DD HH:mm");
+        } else {
+            content.seenAt = "";
+        }
+        return content;
+    }));
+
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_CATEGORY_CONTENT_SUCCESS, result));
+});
