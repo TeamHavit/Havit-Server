1b9bc0c9be86c2a2d917a41918cc3ada288e3155
Hyosik <hyosik@chartmetric.com>
2023-06-09 17:15:56 -0700

    [REMOVE] 불필요한 주석 삭제

--- functions/api/index.js
+++ functions/api/index.js
@@ -15,24 +15,14 @@ Sentry.init({
   dsn: process.env.SENTRY_DSN,
   environment: `${process.env.NODE_ENV}_app`,
   integrations: [
-    // enable HTTP calls tracing
     new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
     new Sentry.Integrations.Express({ app }),
-    // Automatically instrument Node.js libraries and frameworks
     ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
   ],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
   tracesSampleRate: process.env.SENTRY_TRACES_SAMPLE_RATE,
 });
 
-// RequestHandler creates a separate execution context, so that all
-// transactions/spans/breadcrumbs are isolated across requests
 app.use(Sentry.Handlers.requestHandler());
-// TracingHandler creates a trace for every incoming request
 app.use(Sentry.Handlers.tracingHandler());
 
 
