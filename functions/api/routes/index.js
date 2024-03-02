973c13dec752432be90ba71a963be98975dc99c6
jokj624 <jokj624@gmail.com>
2024-03-02 22:59:27 +0900

    [FEAT] swagger 명세 샘플 추가

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -1,12 +1,118 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/content', require('./content'));
-router.use('/category', require('./category'));
-router.use('/recommendation', require('./recommendation'));
-router.use('/user', require('./user'));
-router.use('/auth', require('./auth'));
-router.use('/notice', require('./notice'));
-router.use('/health', require('./health'));
+router.use(
+    '/content', require('./content') 
+    /** 
+     * #swagger.tags = ['content'] 
+     * #swagger.responses[500] = {
+           description: "Internal Server Error",
+           content: {
+               "application/json": {
+                   schema:{
+                       $ref: "#/components/schemas/internalServerErrorSchema"
+                   }
+               }           
+           }
+       }   
+   */ 
+);
+router.use(
+    '/category', require('./category')
+    /** 
+     * #swagger.tags = ['category'] 
+     * #swagger.responses[500] = {
+           description: "Internal Server Error",
+           content: {
+               "application/json": {
+                   schema:{
+                       $ref: "#/components/schemas/internalServerErrorSchema"
+                   }
+               }           
+           }
+       }   
+   */
+);
+router.use(
+    '/recommendation', require('./recommendation')
+    /** 
+     * #swagger.tags = ['recommendation']
+     * #swagger.responses[500] = {
+           description: "Internal Server Error",
+           content: {
+               "application/json": {
+                   schema:{
+                       $ref: "#/components/schemas/internalServerErrorSchema"
+                   }
+               }           
+           }
+       }   
+   */
+);
+router.use(
+    '/user', require('./user')
+    /** 
+     * #swagger.tags = ['user'] 
+     * #swagger.responses[500] = {
+           description: "Internal Server Error",
+           content: {
+               "application/json": {
+                   schema:{
+                       $ref: "#/components/schemas/internalServerErrorSchema"
+                   }
+               }           
+           }
+       }   
+   */
+);
+router.use(
+    '/auth', require('./auth')
+    /** 
+     * #swagger.tags = ['auth'] 
+     * #swagger.responses[500] = {
+           description: "Internal Server Error",
+           content: {
+               "application/json": {
+                   schema:{
+                       $ref: "#/components/schemas/internalServerErrorSchema"
+                   }
+               }           
+           }
+       }   
+   */
+);
+router.use(
+    '/notice', require('./notice')
+    /** 
+     * #swagger.tags = ['notice'] 
+     * #swagger.responses[500] = {
+           description: "Internal Server Error",
+           content: {
+               "application/json": {
+                   schema:{
+                       $ref: "#/components/schemas/internalServerErrorSchema"
+                   }
+               }           
+           }
+       }   
+   */
+    
+);
+router.use(
+    '/health', require('./health')
+    /** 
+     * #swagger.tags = ['health'] 
+     * #swagger.responses[500] = {
+           description: "Internal Server Error",
+           content: {
+               "application/json": {
+                   schema:{
+                       $ref: "#/components/schemas/internalServerErrorSchema"
+                   }
+               }           
+           }
+       }   
+   */
+);
 
 module.exports = router;
