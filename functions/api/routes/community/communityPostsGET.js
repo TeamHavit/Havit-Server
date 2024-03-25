b9746b47d7204b97ace52a09c7c16f0c4d575359
yubinquitous <yb9854@naver.com>
2024-03-26 00:52:19 +0900

    [FIX] query string 예외 처리 및 주석 추가

--- functions/api/routes/community/communityPostsGET.js
+++ functions/api/routes/community/communityPostsGET.js
@@ -16,25 +16,37 @@ const dummyImages = require('../../../constants/dummyImages');
 
 module.exports = asyncWrapper(async (req, res) => {
   const { page, limit } = req.query;
+  // page, limit이 없는 경우
   if (!page || !limit) {
-    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  }
+  // page, limit이 양수가 아닌 경우
+  if (isNaN(page) || isNaN(limit) || page < 1 || limit < 1) {
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, responseMessage.OUT_OF_VALUE));
   }
 
-  // db 연결
   const dbConnection = await db.connect(req);
   req.dbConnection = dbConnection;
 
   dayjs().format();
   dayjs.extend(customParseFormat);
 
-  const totalItemCount = await communityDB.getCommunityPostsCount(dbConnection);
-  const totalPageCount = Math.ceil(totalItemCount / limit);
-  const isLastPage = totalPageCount === page;
+  const totalItemCount = parseInt(await communityDB.getCommunityPostsCount(dbConnection), 10); // 총 게시글 수
+  const totalPageCount = Math.ceil(totalItemCount / limit); // 총 페이지 수
+  const isLastPage = totalPageCount === page; // 마지막 페이지인지 여부
+  // 요청한 페이지가 존재하지 않는 경우
   if (page > totalPageCount) {
-    return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, responseMessage.NO_PAGE));
+    return res
+      .status(statusCode.NOT_FOUND)
+      .send(util.fail(statusCode.NOT_FOUND, responseMessage.NO_PAGE));
   }
 
   const communityPosts = await communityDB.getCommunityPosts(dbConnection, limit, page);
+  // 각 게시글의 createdAt 형식 변경 및 프로필 이미지 추가
   const result = await Promise.all(
     communityPosts.map((communityPost) => {
       communityPost.createdAt = dayjs(`${communityPost.createdAt}`).format('YYYY. MM. DD');
@@ -46,7 +58,7 @@ module.exports = asyncWrapper(async (req, res) => {
   res.status(statusCode.OK).send(
     util.success(statusCode.OK, responseMessage.READ_COMMUNITY_POSTS_SUCCESS, {
       posts: result,
-      currentPage: page,
+      currentPage: parseInt(page, 10),
       totalPageCount,
       totalItemCount,
       isLastPage,
