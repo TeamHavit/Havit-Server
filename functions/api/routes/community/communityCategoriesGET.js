90d5657a186b69ac95cd81b4c59acef4293f6299
Hyosik Philip Joo <lux1937@gmail.com>
2024-03-31 03:15:23 +0900

    [FEAT] 커뮤니티 카테고리 조회 성공 API 구현

--- functions/api/routes/community/communityCategoriesGET.js
+++ functions/api/routes/community/communityCategoriesGET.js
@@ -1,9 +1,23 @@
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const db = require('../../../db/db');
+const { communityDB } = require('../../../db');
+const asyncWrapper = require('../../../lib/asyncWrapper');
+
 /**
  *  @route GET /community/categories
  *  @desc 커뮤니티 카테고리 조회
  *  @access Public
  */
 
-module.exports = async (req, res) => {
+module.exports = asyncWrapper(async (req, res) => {
+  const dbConnection = await db.connect(req);
+  req.dbConnection = dbConnection;
 
-};
+  const communityCategories = await communityDB.getCommunityCategories(dbConnection);
+
+  res.status(statusCode.OK).send(
+    util.success(statusCode.OK, responseMessage.READ_COMMUNITY_CATEGORY_SUCCESS, communityCategories),
+  );
+});
