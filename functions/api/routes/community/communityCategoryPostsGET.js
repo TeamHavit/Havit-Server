b45ce8721a3fa4a23fb93555e6ea58bcfb4a9f27
yubinquitous <yb9854@naver.com>
2024-04-04 10:36:40 +0900

    [WIP] 커뮤니티 카테고리별 게시글 조회 구현 중

--- functions/api/routes/community/communityCategoryPostsGET.js
+++ functions/api/routes/community/communityCategoryPostsGET.js
@@ -1,9 +1,49 @@
+const dayjs = require('dayjs');
+const customParseFormat = require('dayjs/plugin/customParseFormat');
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const asyncWrapper = require('../../../lib/asyncWrapper');
+const db = require('../../../db/db');
+const { communityDB } = require('../../../db');
+const dummyImages = require('../../../constants/dummyImages');
 /**
  *  @route GET /community/categories/:communityCategoryId
  *  @desc 커뮤니티 카테고리별 게시글 조회
  *  @access Private
  */
 
-module.exports = async (req, res) => {
+module.exports = asyncWrapper(async (req, res) => {
+  const { userId } = req.user;
   const { page, limit } = req.query;
-};
+  const { communityCategoryId } = req.params;
+
+  const dbConnection = await db.connect(req);
+  req.dbConnection = dbConnection;
+
+  dayjs().format();
+  dayjs.extend(customParseFormat);
+
+  const totalItemCount = await communityDB.getCommunityCategoryPostsCount(
+    dbConnection,
+    userId,
+    communityCategoryId,
+  ); // 총 게시글 수
+  const totalPageCount = Math.ceil(totalItemCount / limit); // 총 페이지 수
+  const currentPage = +page; // 현재 페이지
+  const isLastPage = totalPageCount === currentPage; // 마지막 페이지인지 여부
+  // 요청한 페이지가 존재하지 않는 경우
+  if (page > totalPageCount) {
+    return res
+      .status(statusCode.NOT_FOUND)
+      .send(util.fail(statusCode.NOT_FOUND, responseMessage.NO_PAGE));
+  }
+
+  const communityCategoryPosts = await communityDB.getCommunityCategoryPostsByCommunityCategoryId(
+    dbConnection,
+    userId,
+    communityCategoryId,
+    page,
+    limit,
+  );
+});
