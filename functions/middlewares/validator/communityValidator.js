91477d19493cf4ec2c8566c2134ba366dc072479
Hyosik Philip Joo <lux1937@gmail.com>
2024-04-18 16:18:29 +0900

    [FEAT] 커뮤니티 게시글 신고하기 validator 구현

--- functions/middlewares/validator/communityValidator.js
+++ functions/middlewares/validator/communityValidator.js
@@ -25,8 +25,13 @@ const getCommunityCategoryPostsValidator = [
     .withMessage('Invalid communityCategoryId field'),
 ];
 
+const reportCommunityPostValidator = [
+  body('communityPostId').isInt({ min: 0 }).notEmpty().withMessage('Invalid communityPostId')
+]
+
 module.exports = {
   createCommunityPostValidator,
   getCommunityPostsValidator,
   getCommunityCategoryPostsValidator,
+  reportCommunityPostValidator
 };
