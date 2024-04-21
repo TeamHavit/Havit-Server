15c9fd3cbe740e537bee8a52735a1584e04c2eda
Hyosik Philip Joo <lux1937@gmail.com>
2024-04-21 18:56:42 +0900

    [FEAT] 커뮤니티 게시글 신고하기 controller 구현

--- functions/api/routes/community/communityReportPOST.js
+++ functions/api/routes/community/communityReportPOST.js
@@ -1,9 +1,27 @@
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const db = require('../../../db/db');
+const { communityDB } = require('../../../db');
+const asyncWrapper = require('../../../lib/asyncWrapper');
+
 /**
  *  @route POST /community/reports
  *  @desc 커뮤니티 게시글 신고
  *  @access Private
  */
 
-module.exports = async (req, res) => {
+module.exports = asyncWrapper(async (req, res) => {
+  const { userId } = req.user;
   const { communityPostId } = req.body;
-};
+
+  const dbConnection = await db.connect(req);
+  req.dbConnection = dbConnection;
+
+  const communityPostReport = await communityDB.reportCommunityPost(dbConnection, userId, communityPostId);
+  if (!communityPostReport) {
+    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_COMMUNITY_POST))
+  }
+  
+  res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, responseMessage.REPORT_COMMUNITY_POST_SUCCESS))
+});
