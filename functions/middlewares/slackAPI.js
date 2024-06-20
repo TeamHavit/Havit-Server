b58a44466c9ab96c869a853704600cda8c29dcae
jokj624 <jokj624@gmail.com>
2024-06-21 00:22:03 +0900

    [FIX] slack message block/text type에 따른 body 수정

--- functions/middlewares/slackAPI.js
+++ functions/middlewares/slackAPI.js
@@ -5,25 +5,31 @@ const axios = require('axios');
 // endpoint 자체는 깃허브에 올라가면 안 되기 때문!
 const WEB_HOOK_ERROR_MONITORING = process.env.WEB_HOOK_ERROR_MONITORING;
 
-const sendMessageToSlack = (message, apiEndPoint = WEB_HOOK_ERROR_MONITORING) => {
-    // 슬랙 Webhook을 이용해 슬랙에 메시지를 보내는 코드
-    try {
-        axios
-            .post(apiEndPoint, { text: message })
-            .then((response) => { })
-            .catch((e) => {
-                throw e;
-            });
-    } catch (e) {
-        console.error(e);
-        // 슬랙 Webhook 자체에서 에러가 났을 경우,
-        // Firebase 콘솔에 에러를 찍는 코드
-        functions.logger.error('[slackAPI 에러]', { error: e });
-    }
+const sendMessageToSlack = (
+  message,
+  apiEndPoint = WEB_HOOK_ERROR_MONITORING,
+  isBlockMessage = false,
+) => {
+  // 슬랙 Webhook을 이용해 슬랙에 메시지를 보내는 코드
+  const body = isBlockMessage ? message : { text: message };
+
+  try {
+    axios
+      .post(apiEndPoint, body)
+      .then((response) => {})
+      .catch((e) => {
+        throw e;
+      });
+  } catch (e) {
+    console.error(e);
+    // 슬랙 Webhook 자체에서 에러가 났을 경우,
+    // Firebase 콘솔에 에러를 찍는 코드
+    functions.logger.error('[slackAPI 에러]', { error: e });
+  }
 };
 
 // 이 파일에서 정의한 변수 / 함수를 export 해서, 다른 곳에서 사용할 수 있게 해주는 코드
 module.exports = {
-    sendMessageToSlack,
-    WEB_HOOK_ERROR_MONITORING,
+  sendMessageToSlack,
+  WEB_HOOK_ERROR_MONITORING,
 };
